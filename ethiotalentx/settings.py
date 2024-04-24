de471bee9ccf827734a5c76bce311a18f09f0e66
ab3nxy <abenxy0@gmail.com>
2024-04-25 01:57:54 +0300

    Landing page finished

--- ethiotalentx/settings.py
+++ ethiotalentx/settings.py
@@ -42,9 +42,6 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 #crispy form 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-LOGIN_REDIRECT_URL = '/index.html'
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -165,7 +162,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') # 'data' is my media folder
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- ethiotalentx/urls.py
+++ ethiotalentx/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views 
+from . import views
+from django.conf import settings
+from django.urls import re_path
+from  django.views.static import serve 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('accounts/', include('allauth.urls')),
     path('', include('allauth.socialaccount.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
