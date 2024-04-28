40e30350291fcf01611184e203477cfddfc98667
ab3nxy <abenxy0@gmail.com>
2024-04-28 17:56:33 +0300

    Deployment allowed host updated

--- ethiotalentx/settings.py
+++ ethiotalentx/settings.py
@@ -37,7 +37,7 @@ class Dev(Configuration):
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
 
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ethiotalenthub.onrender.com']
 
     #Email from gmail
     EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
