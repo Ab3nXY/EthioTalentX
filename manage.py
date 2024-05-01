268fa3f1b5b519cf29924d52ff237145db8c447c
ab3nxy <abenxy0@gmail.com>
2024-05-01 21:11:38 +0300

    deployment 5/1/2024

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ethiotalentx.settings')
-    os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
     try:
         from configurations.management import execute_from_command_line
     except ImportError as exc:
