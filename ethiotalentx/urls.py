541038b435f809a778c6e65353fd1a8afbe8eb83
ab3nxy <abenxy0@gmail.com>
2024-05-04 16:09:08 +0300

    Deployment 5/4/2024

--- ethiotalentx/urls.py
+++ ethiotalentx/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
 ]
 
 # Serve static and media files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
