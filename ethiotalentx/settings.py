11ffa8bc79949f95129427da9ca37040c9a831c8
ab3nxy <abenxy0@gmail.com>
2024-05-26 22:09:00 +0300

    Update for final deployment in render

--- ethiotalentx/settings.py
+++ ethiotalentx/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = ['ethiotalenthub.onrender.com']
+ALLOWED_HOSTS = ['ethiotalenthub.onrender.com', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ["https://ethiotalenthub.onrender.com"]
 
 #Email from gmail
@@ -162,15 +162,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-#
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') 
+# Media files (Uploaded by users)
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -180,7 +178,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
 
-DEBUG = True
+DEBUG = False
 
 # # for pythonanywhere
 # DATABASES = {
--- ethiotalentx/urls.py
+++ ethiotalentx/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts import views
 from django.conf import settings
-from django.urls import re_path
-from  django.views.static import serve 
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -13,10 +10,9 @@ urlpatterns = [
     path('', include('donations.urls')),
     path('accounts/', include('allauth.urls')),
     path('', include('allauth.socialaccount.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
 
-# Serve static and media files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve static and media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
