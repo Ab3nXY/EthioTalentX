1ae12a9d9c26d92d90051694db05ba6b6ea90de9
ab3nxy <abenxy0@gmail.com>
2024-05-26 22:50:54 +0300

    Update for final deployment in render 3

--- ethiotalentx/settings.py
+++ ethiotalentx/settings.py
@@ -75,7 +75,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -171,7 +170,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- ethiotalentx/wsgi.py
+++ ethiotalentx/wsgi.py
@@ -8,14 +8,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
-from django.core.wsgi import get_wsgi_application
-from pathlib import Path
 
-# Define BASE_DIR to reference the project's root directory
-BASE_DIR = Path(__file__).resolve().parent.parent
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ethiotalentx.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
